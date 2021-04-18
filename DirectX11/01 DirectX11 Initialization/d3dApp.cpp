cdbe0d733d618c39270edbc07aecc83147ac43db
huajian <1415885403@qq.com>
2021-04-19 01:40:01 +0800

    add file

---
+++ DirectX11/01 DirectX11 Initialization/d3dApp.cpp
@@ -0,0 +1,34 @@
+#include "d3dApp.h"
+#include "d3dUtil.h"
+#include "DXTrace.h"
+
+namespace
+{
+	// This is just used to forward Windows messages from a global window
+	// procedure to our member function window procedure because we cannot
+	// assign a member function to WNDCLASS::lpfnWndProc.
+	D3DApp* g_pd3dApp = 0;
+}
+
+
+D3DApp::D3DApp(HINSTANCE hInstance)
+	:m_hAppInst(hInstance),
+	m_MainWndCaption(L"DirectX11 Initialization"),
+	m_ClientWidth(800),
+	m_ClientHeight(600),
+	m_hMainWnd(nullptr),
+	m_AppPaused(false),
+	m_Minimized(false),
+	m_Maximized(false),
+	m_Resizing(false),
+	m_Enable4xMsaa(true),
+	m_4xMsaaQuality(0),
+	m_pd3dDevice(nullptr),
+	m_pd3dImmediateContext(nullptr),
+	m_pSwapChain(nullptr),
+	m_pDepthStencilBuffer(nullptr),
+	m_pRenderTargetView(nullptr),
+	m_pDepthStencilView(nullptr)
+{
+	ZeroMemory(&m_ScreenViewport, sizeof(D3D11_VIEWPORT));
+}
